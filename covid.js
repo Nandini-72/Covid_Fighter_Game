d6a3a5198dff0ea3e21d52eaecbe4b6c6921257c
Nandini <jainnandini741@gmail.com>
2020-06-24 13:21:15 +0530

    player added

--- covid.js
+++ covid.js
@@ -3,6 +3,12 @@ function load_images(){
     enemy_img = new Image();
     enemy_img.src = "Assets/enemy.png";
     
+    player_img = new Image();
+    player_img.src = "Assets/fighter.png";
+    
+    gem_img = new Image;
+    gem_img.src = "Assets/vac1.png";
+    
 }
 function init(){
     cvs = document.getElementById('mycanvas');
@@ -42,6 +48,31 @@ function init(){
     };
     
     enemy = [e1,e2,e3,e4];
+    player = {
+        x:20,
+        y:H/2,
+        w:150,
+        h:150,
+        speed:30,
+        moving:"false"
+    }
+    gem = {
+        x:W-100,
+        y:H/2,
+        w:100,
+        h:100,
+    }
+    document.addEventListener('keydown',function(e){
+        console.log("HEy")
+        if(e.key == "ArrowRight"){
+            console.log("Pressed");
+            player.moving = true;
+        }
+        if(e.key == "ArrowLeft"){
+            console.log("Relaessed");
+            player.moving = false;
+        }
+    })
 }
 function draw(){
    pen.clearRect(0,0,W,H);
@@ -50,13 +81,20 @@ function draw(){
     for(let i = 0; i<enemy.length; i++){
      pen.drawImage(enemy_img,enemy[i].x,enemy[i].y,enemy[i].w,enemy[i].h);
     }
-    
+    // draw the player
+    pen.drawImage(player_img,player.x,player.y,player.w,player.h);
 }
 function update(){
 //    box.y += box.speed;
 //    if(box.y > H-box.h || box.y <= 0){
 //        box.speed   *= -1;
 //    }
+    // if player is moving
+    if(player.moving == true){
+        player.x += player.speed;
+        
+    }
+    
     for(let i = 0; i<enemy.length; i++){
         enemy[i].y += enemy[i].speed;
         if(enemy[i].y > H-enemy[i].h || enemy[i].y <= 0){
